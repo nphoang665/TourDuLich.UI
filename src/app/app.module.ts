dd6c74ea5aef13a09f51328f919c1978aa323b57
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2024-03-05 21:30:22 +0700

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Pages/GiaoDienKhachHang/Header/header/header.component';
+import { FooterComponent } from './Pages/GiaoDienKhachHang/Header/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
