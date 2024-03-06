6b04038d3cede27154af85faa78c4f2f1c0a0553
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2024-03-06 09:16:44 +0700

    done header fixed

--- src/app/Pages/GiaoDienKhachHang/TrangChu/trang-chu/trang-chu.component.ts
+++ src/app/Pages/GiaoDienKhachHang/TrangChu/trang-chu/trang-chu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-trang-chu',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './trang-chu.component.css'
 })
 export class TrangChuComponent {
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let originalHeader = document.querySelector('.container_large_header') as HTMLElement;
+    let fixedHeader = document.querySelector('.container_large_header') as HTMLElement;
+    let itemHeaders = document.querySelectorAll('.item_medium_second_header');
 
+    itemHeaders.forEach((item: Element) => {
+      let itemHeader = item as HTMLElement;
+      if (window.pageYOffset > originalHeader.offsetTop) {
+        fixedHeader.classList.add('fixedHeader');
+        itemHeader.classList.add('text-dark');
+      } else {
+        fixedHeader.classList.remove('fixedHeader');
+        itemHeader.classList.remove('text-dark');
+      }
+    });
+
+  }
 }
