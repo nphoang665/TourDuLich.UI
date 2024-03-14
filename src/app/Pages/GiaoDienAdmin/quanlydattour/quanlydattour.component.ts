b9f7b59d48dea8df045afc354027f93826184335
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2024-03-14 11:04:32 +0700

    quanlydattour

--- src/app/Pages/GiaoDienAdmin/quanlydattour/quanlydattour.component.ts
+++ src/app/Pages/GiaoDienAdmin/quanlydattour/quanlydattour.component.ts
@@ -31,12 +31,14 @@ export class QuanlydattourComponent implements OnInit {
       this.TourDuLich.forEach(element => {
         var urlFirstImgTour = environment.apiBaseUrl + '/uploads/' + element.anhTour[0].imgTour;
         element.AnhTourDauTien = urlFirstImgTour;
+        element.SoNgayDem = this.calculateDaysAndNights(element.thoiGianBatDau, element.thoiGianKetThuc);
 
 
       });
       console.log(this.TourDuLich);
 
       this.Fnc_TinhNgayDem();
+
     });
     //lấy khách hàng từ db
     this.khachHang$ = this.quanLyKhachHangServices.getAllTourKhachHang();
@@ -74,6 +76,77 @@ export class QuanlydattourComponent implements OnInit {
 
 
   }
-  //chuyển tab pane
+  TourDuLichById: any;
+  TenNhanVien: any;
+  model?: TourDuLich;
+  NgayDatTour!: string;
+  SoLuongNguoiLon_DatTour: number = 1;
+  SoLuongTreEm_DatTour: number = 0;
+  SoNgayDem: string = '';
+  TongTien_DatTour!: number;
+  //hàm lấy tour theo id
+  LayTourDuLich(idTour: string) {
+    this.quanLyTourService.getTourDuLichById(idTour).subscribe((data: TourDuLich) => {
+
+      this.TenNhanVien = 'NV0001';
+
+      this.model = data;
+
+
+      let ngayThem: Date = new Date();
+      let year: string = ngayThem.getFullYear().toString();
+      let month: string = (ngayThem.getMonth() + 1).toString().padStart(2, '0'); // padStart để thêm số 0 phía trước nếu tháng chỉ có 1 chữ số
+      let date: string = ngayThem.getDate().toString().padStart(2, '0'); // tương tự như trên
+      let hours: string = ngayThem.getHours().toString().padStart(2, '0');
+      let minutes: string = ngayThem.getMinutes().toString().padStart(2, '0');
+      let NgayGioConvert: string = `${year}-${month}-${date}T${hours}:${minutes}`;
+      this.NgayDatTour = NgayGioConvert;
+      this.SoNgayDem = this.calculateDaysAndNights(this.model.thoiGianBatDau, this.model.thoiGianKetThuc);
+      this.TinhTongTien();
+
+    })
+  }
+  //hàm tính toán ngày đêm
+  calculateDaysAndNights(thoiGianBatDau: any, thoiGianKetThuc: any): string {
+    // Check if inputs are Date objects, if not convert them
+    let startDate = thoiGianBatDau instanceof Date ? thoiGianBatDau : new Date(thoiGianBatDau);
+    let endDate = thoiGianKetThuc instanceof Date ? thoiGianKetThuc : new Date(thoiGianKetThuc);
+
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const diffDays = Math.round(Math.abs((startDate.getTime() - endDate.getTime()) / oneDay));
+    return `${diffDays} ngày ${diffDays - 1} đêm`;
+  }
+  //hàm xử lý tính toán tổng tiền
+  TinhTongTien() {
+    this.TongTien_DatTour = (this.SoLuongNguoiLon_DatTour * Number(this.model?.giaNguoiLon)) + (this.SoLuongTreEm_DatTour * Number(this.model?.giaTreEm));
+
+  }
+  //hàm tính toán số lượng người dự tour
+  TinhSoLuongNguoiDuTour(loaiNguoi: string, kieuNutBam: string) {
+    //nếu là loaiNguoi = người lớn
+
+    //nếu kiểu nút bấm là  + 
+    if (loaiNguoi === "NguoiLon" && kieuNutBam === "Cong") {
+      this.SoLuongNguoiLon_DatTour++;
+    }
+    //nếu kiểu nút bấm là  -
+    else if (loaiNguoi === "NguoiLon" && kieuNutBam === "Tru") {
+      this.SoLuongNguoiLon_DatTour--;
+    }
+
+    //nếu là loaiNguoi = trẻ em
+
+    //nếu kiểu nút bấm là  + 
+    else if (loaiNguoi === "TreEm" && kieuNutBam === "Cong") {
+      this.SoLuongTreEm_DatTour++;
+    }
+    //nếu kiểu nút bấm là  -
+    else {
+      this.SoLuongTreEm_DatTour--;
+    }
+
+  }
+
+
 
 }
