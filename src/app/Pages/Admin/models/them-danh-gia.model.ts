10758a0ebc893b72d8f8f5acf9ec5e77d984f6b9
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2024-03-24 23:05:31 +0700

    danhgia

---
+++ src/app/Pages/Admin/models/them-danh-gia.model.ts
@@ -0,0 +1,10 @@
+export interface ThemDanhGia {
+    idDanhGia: string;
+    idKhachHang: string;
+    idTour: string | null;
+    diemDanhGia: number;
+    nhanXet: string;
+    thoiGianDanhGia: Date;
+
+
+}
