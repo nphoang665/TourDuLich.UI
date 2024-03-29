186725c863a9763b967fbfe70cff1494816c7dff
AnhThien13 <thienbmt1310@gmail.com>
2024-03-29 22:27:56 +0700

    validationThemtour

--- src/app/Pages/Admin/quan-ly-tour/them-tour/them-tour.component.ts
+++ src/app/Pages/Admin/quan-ly-tour/them-tour/them-tour.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { themTour } from '../../models/them-tour.model';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -9,7 +9,15 @@ import { DoiTac } from '../../models/doi-tac.model';
 import { QuanLyTourService } from '../../services/quan-ly-tour.service';
 import { DoiTacService } from '../../services/DoiTac/doi-tac.service';
 import { ChangeEventArgs, DateTimePicker } from '@syncfusion/ej2-calendars';
-
+import { FormsModule, ValidatorFn, } from '@angular/forms';
+import { ReactiveFormsModule,  Validator, AbstractControl } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-them-tour',
   templateUrl: './them-tour.component.html',
@@ -17,27 +25,126 @@ import { ChangeEventArgs, DateTimePicker } from '@syncfusion/ej2-calendars';
 })
 export class ThemTourComponent implements OnInit, OnDestroy {
   submitted = false;
+  matcher = new MyErrorStateMatcher();
   //form group
   ThemTourForm: FormGroup = new FormGroup({
     idTour: new FormControl('123'),
-    tenTour: new FormControl(''),
-    loaiTour: new FormControl(''),
-    phuongTienDiChuyen: new FormControl(''),
-    moTa: new FormControl(''),
-    soLuongNguoiLon: new FormControl(0),
-    soLuongTreEm: new FormControl(0),
-    thoiGianBatDau: new FormControl(''),
-    thoiGianKetThuc: new FormControl(''),
-    noiKhoiHanh: new FormControl(''),
-    soChoConNhan: new FormControl(0),
-    idDoiTac: new FormControl(''),
-    giaTreEm: new FormControl(0),
-    giaNguoiLon: new FormControl(0),
+    tenTour: new FormControl('',[
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(50),
+      this.noSpecialCharValidator(),
+    ]),
+    loaiTour: new FormControl('',
+     Validators.required),
+    phuongTienDiChuyen: new FormControl('',
+    Validators.required),
+    moTa: new FormControl('',
+    Validators.required),
+    soLuongNguoiLon: new FormControl(0,[
+      Validators.required,
+      Validators.min(0),
+      Validators.max(50),
+      this.noSpecialCharValidator(),
+    ]),
+    soLuongTreEm: new FormControl(0,[
+      Validators.required,
+      Validators.min(0),
+      Validators.max(50),
+      this.noSpecialCharValidator(),
+    ]),
+    thoiGianBatDau: new FormControl('',
+    Validators.required),
+    thoiGianKetThuc: new FormControl('',
+    Validators.required),
+    noiKhoiHanh: new FormControl('',[
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50)
+    ]),
+    soChoConNhan: new FormControl(0,[
+      Validators.required,
+      Validators.min(2),
+      Validators.max(50),
+      this.noSpecialCharValidator(),
+    ]),
+    idDoiTac: new FormControl('',
+    Validators.required),
+    giaTreEm: new FormControl(0,[
+      Validators.required,
+      Validators.min(0),
+      Validators.max(250000),
+      this.noSpecialCharValidator(),
+    ]),
+    giaNguoiLon: new FormControl(0,[
+      Validators.required,
+      Validators.min(0),
+      Validators.max(650000),
+      this.noSpecialCharValidator(),
+    ]),
     ngayThem: new FormControl(new Date()),
-    dichVuDiKem: new FormControl(''),
+    dichVuDiKem: new FormControl('',[
+      Validators.required,
+      Validators.min(0),
+      Validators.max(650000),
+      this.noSpecialCharValidator(),
+    ]),
     tinhTrang: new FormControl('z')
   });
-
+  get tenTour(){
+    return this.ThemTourForm.get('tenTour');
+  }
+  get loaiTour(){
+    return this.ThemTourForm.get('loaiTour');
+  }
+  get phuongTienDiChuyen(){
+    return this.ThemTourForm.get('phuongTienDiChuyen');
+  }
+  get moTa(){
+    return this.ThemTourForm.get('moTa');
+  }
+  get soLuongNguoiLon(){
+    return this.ThemTourForm.get('soLuongNguoiLon');
+  }
+  get soLuongTreEm(){
+    return this.ThemTourForm.get('soLuongTreEm');
+  }
+  get thoiGianBatDau(){
+    return this.ThemTourForm.get('thoiGianBatDau');
+  }
+  get thoiGianKetThuc(){
+    return this.ThemTourForm.get('thoiGianKetThuc');
+  }
+  get noiKhoiHanh(){
+    return this.ThemTourForm.get('noiKhoiHanh');
+  }
+  get soChoConNhan(){
+    return this.ThemTourForm.get('soChoConNhan');
+  }
+  get idDoiTac(){
+    return this.ThemTourForm.get('idDoiTac');
+  }
+  get giaTreEm(){
+    return this.ThemTourForm.get('giaTreEm');
+  }
+  get giaNguoiLon(){
+    return this.ThemTourForm.get('giaNguoiLon');
+  }
+  get ngayThem(){
+    return this.ThemTourForm.get('ngayThem');
+  }
+  get dichVuDiKem(){
+    return this.ThemTourForm.get('dichVuDiKem');
+  }
+  get tinhTrang(){
+    return this.ThemTourForm.get('tinhTrang');
+  }
+  noSpecialCharValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const invalidChar = /^[^\d~`!@#$%\^&*()_+=\-\[\]\\';,/{}|\\":<>\?]*$/.test(control.value);
+      return invalidChar ? null : { 'invalidChar': { value: control.value } };
+    };
+  }
 
 
   private addTourSubscribtion?: Subscription;
