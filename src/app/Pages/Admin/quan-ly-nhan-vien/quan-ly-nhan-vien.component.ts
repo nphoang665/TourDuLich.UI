01d425ab0e69ff69c55126ac3beba7693f930170
nphoang665 <phihoang665@gmail.com>
2024-04-01 11:02:32 +0700

    #Form nhanvien

--- src/app/Pages/Admin/quan-ly-nhan-vien/quan-ly-nhan-vien.component.ts
+++ src/app/Pages/Admin/quan-ly-nhan-vien/quan-ly-nhan-vien.component.ts
@@ -1,40 +1,107 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NhanVienService } from '../services/NhanVien/nhan-vien.service';
 import { Observable } from 'rxjs';
 import { NhanVien } from '../models/nhan-vien.model';
 import { ToastrService } from 'ngx-toastr';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
+import { ThemNhanVienComponent } from './them-nhan-vien/them-nhan-vien.component';
+import { SuaNhanVienComponent } from './sua-nhan-vien/sua-nhan-vien.component';
 
 @Component({
   selector: 'app-quan-ly-nhan-vien',
   templateUrl: './quan-ly-nhan-vien.component.html',
   styleUrl: './quan-ly-nhan-vien.component.css'
 })
-export class QuanLyNhanVienComponent implements OnInit{
+export class QuanLyNhanVienComponent implements AfterViewInit,OnInit{
+  displayedColumns: string[] = ['idNhanVien', 'tenNhanVien', 'soDienThoai', 'gioiTinh', 'email','ngaySinh', 'action'];
+  dataSource: MatTableDataSource<NhanVien>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  nhanVien$?:Observable<NhanVien[]>;
-
-constructor(private quanLyNhanVienService:NhanVienService,  private toastr: ToastrService){}
-
-
-  ngOnInit(): void {
-    this.nhanVien$ = this.quanLyNhanVienService.getAllNhanVien();
+constructor(
+  private quanLyNhanVienService:NhanVienService,  
+  private toastr: ToastrService,
+  private dialog: MatDialog,
+  ){
+    this.dataSource = new MatTableDataSource<NhanVien>([])
+    this.getNhanVienData();
   }
 
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
-
-  XoaNhanVien(id: string) {
-    if (id) {
-     this.quanLyNhanVienService
-        .xoaNhanVien(id)
-        .subscribe({
-          next: (response) => {
-            this.nhanVien$ = this.quanLyNhanVienService.getAllNhanVien();
-            this.toastr.success('Xóa nhân viên thành công', 'Thông báo', {
-              timeOut: 1000,
-            });
-          }
-        });
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
     }
+  }
+
+  ngOnInit(): void {
+    // this.nhanVien$ = this.quanLyNhanVienService.getAllNhanVien();
+  }
+  getNhanVienData() {
+    this.quanLyNhanVienService.getAllNhanVien().subscribe(
+      (data: NhanVien[]) => {
+        this.dataSource.data = data.filter(nhanVien => nhanVien.tinhTrang === 'Đang hoạt động');
+      },
+      (error) => {
+        console.error('Error fetching Nhân viên data: ', error);
+      }
+    );
+  }
+
+  OpenPopup(id: any, title: any): void {
+    const _popup = this.dialog.open(ThemNhanVienComponent, {
+      width: '40%',
+      enterAnimationDuration: '250ms',
+      exitAnimationDuration: '250ms',
+      data: {
+        title: title,
+        idNhanVien: id 
+      },
+    });
+    _popup.afterClosed().subscribe((item) => {
+      console.log(item);
+      this.getNhanVienData();
+    });
+  }
+
+  OpenPopupSua(id: any, title: any): void {
+    const _popup = this.dialog.open(SuaNhanVienComponent, {
+      width: '40%',
+      enterAnimationDuration: '250ms',
+      exitAnimationDuration: '250ms',
+      data: {
+        title: title,
+        idNhanVien: id 
+      },
+    });
+    _popup.afterClosed().subscribe((item) => {
+      console.log(item);
+      this.getNhanVienData(); 
+    });
+  }
+
+  themNhanVien():void{
+    this.OpenPopup(0,'Thêm nhân viên')
+  }
+  suaNhanVien(id:any):void{
+    this.OpenPopupSua(id,'Sửa nhân viên')
+  }
+  XoaNhanVien(id:string){
+    this.quanLyNhanVienService.xoaNhanVien(id).subscribe((data: any) => {
+      this.toastr.success('Xóa nhân viên thành công', 'Thông báo', {
+        timeOut: 1000,
+      });
+      this.getNhanVienData();
+    });
   }
     
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import { MAT_DATE_FORMATS, provideNativeDateAdapter } from '@angular/material/core';
 import { SanPhamGiaoDienComponent } from './Pages/GiaoDienKhachHang/SanPhamGiaoDien/san-pham-giao-dien/san-pham-giao-dien.component';
 import { ChiTietTourComponent } from './Pages/GiaoDienKhachHang/ChiTietTour/chi-tiet-tour/chi-tiet-tour.component';
 import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
@@ -62,6 +62,18 @@ import { QuanLyDichVuComponent } from './Pages/Admin/quan-ly-dich-vu/quan-ly-dic
 import { ThemDichVuComponent } from './Pages/Admin/quan-ly-dich-vu/them-dich-vu/them-dich-vu.component';
 import { SuaDichVuComponent } from './Pages/Admin/quan-ly-dich-vu/sua-dich-vu/sua-dich-vu.component';
 import { ThemTourComponent } from './Pages/Admin/quan-ly-tour/them-tour/them-tour.component';
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MM YYYY',
+    dateA11yLabel: 'DD/MM/YYYY',
+    monthYearA11yLabel: 'MM YYYY',
+  },
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -131,6 +143,7 @@ import { ThemTourComponent } from './Pages/Admin/quan-ly-tour/them-tour/them-tou
 
   ],
   providers: [
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     provideClientHydration(),
     provideAnimationsAsync(),
@@ -164,6 +177,6 @@ import { ThemTourComponent } from './Pages/Admin/quan-ly-tour/them-tour/them-tou
       } as SocialAuthServiceConfig,
     }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
