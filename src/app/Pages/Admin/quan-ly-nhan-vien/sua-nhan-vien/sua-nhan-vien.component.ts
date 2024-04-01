f47f2def94664126f54e514c5c959f28b4c9e287
AnhThien13 <thienbmt1310@gmail.com>
2024-04-01 21:49:48 +0700

    validateSuaNV&footer

--- src/app/Pages/Admin/quan-ly-nhan-vien/sua-nhan-vien/sua-nhan-vien.component.ts
+++ src/app/Pages/Admin/quan-ly-nhan-vien/sua-nhan-vien/sua-nhan-vien.component.ts
@@ -1,32 +1,98 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NhanVienService } from '../../services/NhanVien/nhan-vien.service';
 import { NhanVien, SuaNhanVien } from '../../models/nhan-vien.model';
 import { ToastrService } from 'ngx-toastr';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-
+import { FormsModule, ValidatorFn, } from '@angular/forms';
+import { ReactiveFormsModule,  Validator, AbstractControl } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-sua-nhan-vien',
   templateUrl: './sua-nhan-vien.component.html',
   styleUrl: './sua-nhan-vien.component.css'
 })
 export class SuaNhanVienComponent implements OnInit{
+  matcher = new MyErrorStateMatcher();
   suaNhanVienForm: FormGroup = new FormGroup({
-    tenNhanVien:new FormControl(''),
-    soDienThoai:new FormControl(''),
-    diaChi:new FormControl(''),
-    cccd:new FormControl(''),
-    ngaySinh:new FormControl(new Date()),
-    email:new FormControl(''),
-    gioiTinh:new FormControl(''),
-    chucVu:new FormControl(''),
+    tenNhanVien:new FormControl('',[
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(50),
+      this.noSpecialCharValidator(),
+    ]),
+    soDienThoai:new FormControl('',[
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10), 
+    ]),
+    diaChi:new FormControl('',[
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(50), 
+    ]),
+    cccd:new FormControl('',[
+      Validators.required,
+      Validators.minLength(0),
+      Validators.maxLength(12), 
+    ]),
+    ngaySinh:new FormControl(new Date(),
+    Validators.required),
+    email:new FormControl('',[
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(50), 
+    ]),
+    gioiTinh:new FormControl('',
+    Validators.required),
+    chucVu:new FormControl('',
+    Validators.required),
     ngayVaoLam:new FormControl(new Date()),
     anhNhanVien:new FormControl(''),
-    tinhTrang:new FormControl(''),
+    tinhTrang:new FormControl('',
+    Validators.required),
     ngayDangKy:new FormControl(new Date())
   });
-
+  get tenNhanVien(){
+    return this.suaNhanVienForm.get('tenNhanVien');
+  }
+  get soDienThoai(){
+    return this.suaNhanVienForm.get('soDienThoai');
+  }
+  get cccd(){
+    return this.suaNhanVienForm.get('cccd');
+  }
+  get diaChi(){
+    return this.suaNhanVienForm.get('diaChi');
+  }
+  get ngaySinh(){
+    return this.suaNhanVienForm.get('ngaySinh');
+  }
+  get email(){
+    return this.suaNhanVienForm.get('email');
+  }
+  get chucVu(){
+    return this.suaNhanVienForm.get('chucVu');
+  }
+  get tinhTrang(){
+    return this.suaNhanVienForm.get('tinhTrang');
+  }
+  get gioiTinh(){
+    return this.suaNhanVienForm.get('gioiTinh');
+  }
+  noSpecialCharValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const invalidChar = /^[^\d~`!@#$%\^&*()_+=\-\[\]\\';,/{}|\\":<>\?]*$/.test(control.value);
+      return invalidChar ? null : { 'invalidChar': { value: control.value } };
+    };
+  }
   model?:NhanVien;
 
   id?:string | null = null;
