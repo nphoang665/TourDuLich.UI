c7094f1ee93fa8b45c509c26111ebc19441f4add
nphoang665 <phihoang665@gmail.com>
2024-04-10 16:28:43 +0700

    #demoHoaDon

--- src/app/Pages/Admin/services/ThanhToan/thanh-toan.service.ts
+++ src/app/Pages/Admin/services/ThanhToan/thanh-toan.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
-import { ThanhToan } from '../../models/thanh-toan.model';
+import { ThanhToan, ThanhToanDto } from '../../models/thanh-toan.model';
 import { environment } from '../../../../../environments/environment';
+import { DatTour } from '../../models/dat-tour.model';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +19,11 @@ export class ThanhToanService {
     return this.http.get<ThanhToan[]>(`${environment.apiBaseUrl}/api/ThanhToan`);
   }
 
-  getThanhToanById(id:string):Observable<ThanhToan>{
-    return this.http.get<ThanhToan>(`${environment.apiBaseUrl}/api/ThanhToan/${id}`);
+  getThanhToanById(id:string):Observable<ThanhToanDto>{
+    return this.http.get<ThanhToanDto>(`${environment.apiBaseUrl}/api/ThanhToan/${id}`);
   }
-  
+
+  getDatTourById(id:string):Observable<DatTour>{
+    return this.http.get<DatTour>(`${environment.apiBaseUrl}/api/dattour/${id}`);
+  }
 }
