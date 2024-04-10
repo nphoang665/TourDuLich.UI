0e6365efa13127bfde6604535f0c1ce03145f3ea
nphoang665 <phihoang665@gmail.com>
2024-04-10 17:08:21 +0700

    # dichVu, tenTour Chưa lấy được ở  XemThanhToan

--- src/app/Pages/Admin/thanh-toan/xem-thanh-toan/xem-thanh-toan.component.ts
+++ src/app/Pages/Admin/thanh-toan/xem-thanh-toan/xem-thanh-toan.component.ts
@@ -5,6 +5,8 @@ import { ActivatedRoute } from '@angular/router';
 import { ThanhToanService } from '../../services/ThanhToan/thanh-toan.service';
 import { ToastrService } from 'ngx-toastr';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DatTour } from '../../models/dat-tour.model';
+import { QuanLyTourService } from '../../services/quan-ly-tour.service';
 
 @Component({
   selector: 'app-xem-thanh-toan',
@@ -14,12 +16,14 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class XemThanhToanComponent implements OnInit {
   id: string | null = null;
   model?: ThanhToan;
+  datTour?:any;
   thanhToanDto$?:ThanhToanDto;
   thanhToan$?: Observable<ThanhToan[]>;
 
   constructor(
     private route:ActivatedRoute,
     private thanhToanService:ThanhToanService,
+    private quanLyTourService:QuanLyTourService,
     private toastr: ToastrService,
     @Inject(MAT_DIALOG_DATA) public data: any,
 
@@ -38,6 +42,11 @@ export class XemThanhToanComponent implements OnInit {
         this.thanhToanService.getThanhToanById(this.id).subscribe({
           next:(response:ThanhToanDto)=>{
             this.thanhToanDto$ = response;
+            this.thanhToanService.getDatTourById(this.thanhToanDto$.idDatTour).subscribe({
+              next: (datTour: any) => {
+                this.datTour=datTour;
+              }
+            });
           }
         })
        }
